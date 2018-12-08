22c19a1424a30195798cdc403158c1f4908f498f
Martin <ryqiem@gmail.com>
2018-12-08 16:50:09 +0100

    Evaluate quotes and process improvements before moving

--- script.py
+++ script.py
@@ -12,6 +12,8 @@ FORMAT = '%(asctime)-15s, %(levelname)s: %(message)s'
 logging.basicConfig(filename="triage.log", level=logging.INFO, format=FORMAT)
 l = logging.getLogger(__name__)
 
+assert sys.argv[1] is not None
+
 print("Starting script")
 #Setup pytodoist.todoist
 user = todoist.login(USER, PASS)
@@ -31,28 +33,29 @@ def strip_prefix_and_move(task, project):
 def task_to_project(task, project):
     task.move(project)
 
+def categorize(task):
+    if task.content[0:3] == "F: ": #If flashcard
+        threading.Thread(target=strip_prefix_and_move,
+                         args=(task, inbox_flashcards)).start()
+    elif task.content[0:3] == "I: ": #If process improvement
+        threading.Thread(target=strip_prefix_and_move,
+                         args=(task, inbox_process_improvements)).start()
+    else:
+        threading.Thread(target=task_to_project,
+                         args=(task, inbox_tasks)).start()
+
 for task in due_tasks:
     os.system("clear")
     print("\n" * 3)
     print(task.content)
 
-    if task.content[0:3] == "F: ": #If flashcard
-        threading.Thread(target=strip_prefix_and_move,
-                         args=(task, inbox_flashcards)).start()
-        continue
-    elif task.content[0:3] == "I: ": #If process improvement
-        threading.Thread(target=strip_prefix_and_move,
-                         args=(task, inbox_process_improvements)).start()
-        continue
-
     text = input("\n[I]mportant? [N]ecessary? [D]elete? \n\n\n")
 
     if text == "I":
-        threading.Thread(target=task_to_project,
-                         args=(task, inbox_tasks)).start()
+        categorize(task)
+        continue
     elif text == "N":
-        threading.Thread(target=task_to_project,
-                         args=(task, inbox_tasks)).start()
+        categorize(task)
     elif text == "D":
         threading.Thread(target=task.delete).start()
         l.info("Deleted {}".format(task.content))
